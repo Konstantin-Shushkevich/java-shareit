20a48683560fd8f1e5b95e30a15e4d85c13746fe
Konstantin Shushkevich <shusha113@yandex.ru>
2025-02-18 22:24:05 +0300

    Add bookings. For review

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -2,11 +2,9 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     @Query("select i from Item i " +
@@ -16,8 +14,4 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> search(String text);
 
     List<Item> findByOwnerId(Long id);
-
-
-    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.owner WHERE i.id = :id")
-    Optional<Item> findByIdWithOwner(@Param("id") Long id);
 }
